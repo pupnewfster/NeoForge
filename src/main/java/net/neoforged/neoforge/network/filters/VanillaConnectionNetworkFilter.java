efae540f4365d55279357beb0b617f3550edbb64
Sara Freimer <sara@freimer.com>
2026-07-25 12:17:00 -0500

    Fix disconnect from give message

--- src/main/java/net/neoforged/neoforge/network/filters/VanillaConnectionNetworkFilter.java
+++ src/main/java/net/neoforged/neoforge/network/filters/VanillaConnectionNetworkFilter.java
@@ -11,23 +11,35 @@ import io.netty.channel.ChannelHandler;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.synchronization.ArgumentTypeInfo;
 import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.registries.VanillaRegistries;
 import net.minecraft.network.Connection;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.common.ClientboundUpdateTagsPacket;
 import net.minecraft.network.protocol.game.ClientboundCommandsPacket;
+import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
 import net.minecraft.network.protocol.game.ClientboundUpdateAttributesPacket;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagNetworkSerialization;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.item.ItemStackTemplate;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.neoforged.neoforge.network.connection.ConnectionType;
 import net.neoforged.neoforge.registries.RegistryManager;
 import org.jetbrains.annotations.ApiStatus;
@@ -49,6 +61,7 @@ public class VanillaConnectionNetworkFilter extends VanillaPacketFilter {
                 ImmutableMap.<Class<? extends Packet<?>>, BiConsumer<Packet<?>, List<? super Packet<?>>>>builder()
                         .put(handler(ClientboundUpdateAttributesPacket.class, VanillaConnectionNetworkFilter::filterEntityProperties))
                         .put(handler(ClientboundCommandsPacket.class, VanillaConnectionNetworkFilter::filterCommandList))
+                        .put(handler(ClientboundSystemChatPacket.class, VanillaConnectionNetworkFilter::filterSystemChat))
                         .put(handler(ClientboundUpdateTagsPacket.class, VanillaConnectionNetworkFilter::filterCustomTagTypes))
                         .build());
 
@@ -67,12 +80,14 @@ public class VanillaConnectionNetworkFilter extends VanillaPacketFilter {
     private static ClientboundUpdateAttributesPacket filterEntityProperties(ClientboundUpdateAttributesPacket msg) {
         ClientboundUpdateAttributesPacket newPacket = new ClientboundUpdateAttributesPacket(msg.getEntityId(), Collections.emptyList());
         msg.getValues().stream()
-                .filter(snapshot -> {
-                    Identifier key = snapshot.attribute().unwrapKey().map(ResourceKey::identifier).orElse(null);
-                    return key != null && key.getNamespace().equals("minecraft");
-                })
+                .filter(snapshot -> isVanillaAttribute(snapshot.attribute()))
                 .forEach(snapshot -> newPacket.getValues().add(snapshot));
         return newPacket;
+    }
+
+    private static boolean isVanillaAttribute(Holder<Attribute> holder) {
+        Identifier key = holder.unwrapKey().map(ResourceKey::identifier).orElse(null);
+        return key != null && key.getNamespace().equals("minecraft");
     }
 
     /**
@@ -88,6 +103,52 @@ public class VanillaConnectionNetworkFilter extends VanillaPacketFilter {
             return id != null && (id.getNamespace().equals("minecraft") || id.getNamespace().equals("brigadier"));
         });
         return new ClientboundCommandsPacket(newRoot, CommandTreeCleaner.COMMAND_NODE_INSPECTOR);
+    }
+
+    /// Filters out custom attributes from items in components that the client won't recognize.
+    /// A vanilla client would fail to deserialize the packet and disconnect with an error message if these were sent.
+    private static ClientboundSystemChatPacket filterSystemChat(ClientboundSystemChatPacket packet) {
+        Component content = packet.content();
+        if (content.getContents() instanceof TranslatableContents contents) {
+            Object[] args = contents.getArgs();
+            for (int i = 0; i < args.length; i++) {
+                Object arg = args[i];
+                if (arg instanceof Component subArg && subArg.getStyle().getHoverEvent() instanceof HoverEvent.ShowItem(ItemStackTemplate template)) {
+                    ItemAttributeModifiers modifiers = template.get(DataComponents.ATTRIBUTE_MODIFIERS);
+                    if (modifiers != null) {
+                        List<ItemAttributeModifiers.Entry> adjustedModifiers = modifiers.modifiers().stream()
+                                .filter(modifier -> isVanillaAttribute(modifier.attribute()))
+                                .toList();
+                        if (adjustedModifiers.size() != modifiers.modifiers().size()) {
+                            ItemAttributeModifiers.Builder builder = ItemAttributeModifiers.builder();
+                            for (ItemAttributeModifiers.Entry adjustedModifier : adjustedModifiers) {
+                                builder.add(adjustedModifier.attribute(), adjustedModifier.modifier(), adjustedModifier.slot(), adjustedModifier.display());
+                            }
+                            DataComponentPatch.Builder patchBuilder = DataComponentPatch.builder();
+                            for (Map.Entry<DataComponentType<?>, Optional<?>> entry : template.components().entrySet()) {
+                                DataComponentType<?> key = entry.getKey();
+                                Optional<?> value = entry.getValue();
+                                if (value.isPresent()) {
+                                    if (key == DataComponents.ATTRIBUTE_MODIFIERS) {
+                                        patchBuilder.set(DataComponents.ATTRIBUTE_MODIFIERS, builder.build());
+                                    } else {
+                                        patchBuilder.set((DataComponentType) key, value.get());
+                                    }
+                                } else {
+                                    patchBuilder.remove(key);
+                                }
+                            }
+                            ItemStackTemplate adjustedTemplate = new ItemStackTemplate(template.item(), template.count(), patchBuilder.build());
+                            //Abuse the fact that the backing array is mutable
+                            //TODO: Should we just entirely recreate the object?
+                            args[i] = subArg.copy().withStyle(style -> style.withHoverEvent(new HoverEvent.ShowItem(adjustedTemplate)));
+                        }
+                    }
+                }
+            }
+            //return new ClientboundSystemChatPacket(content, packet.overlay());
+        }
+        return packet;
     }
 
     /**
